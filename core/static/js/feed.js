742667c35c4c258df588099f918125568c6c17c1
P4C2UR <dpaczurb@gmail.com>
2023-11-22 17:36:11 +0100

    Capitalize current trend in sort button

--- core/static/js/feed.js
+++ core/static/js/feed.js
@@ -86,6 +86,7 @@ function sortPopupInit() {
   let sort = document.getElementById("sort")
   let button = sort.firstElementChild
   let lis = sort.getElementsByClassName("popup-menu")[0].getElementsByTagName("li")
+  button.textContent = button.textContent.charAt(0).toUpperCase() + button.textContent.slice(1)
   for(let li of lis) {
     if(li.textContent.trim()===button.textContent.trim()) {
       li.style.visibility = "collapse"
