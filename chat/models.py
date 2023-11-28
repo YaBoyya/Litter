2101ada35401ad00412f4c7a22bf237a78573101
YaBoyya <duzniak.amadeusz@gmail.com>
2023-11-28 20:46:12 +0100

    Create ChatRoom and Message models

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class ChatRoom(models.Model):
+    users = models.ManyToManyField('users.LitterUser')
+    title = models.CharField(_("Title"), null=True)
+    is_private = models.BooleanField(default=True)
+
+
+class Message(models.Model):
+    chatroom = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
+    user = models.ForeignKey('users.LitterUser', on_delete=models.DO_NOTHING)
+    text = models.CharField(_("Text"))
+    sent = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
