b165e6d56807c9bfbc4f388f41549371072db974
YaBoyya <duzniak.amadeusz@gmail.com>
2023-12-02 16:49:19 +0100

    Make js messages look the same as db ones

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,6 +2,7 @@ import json
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.utils import timesince
 
 from .models import ChatRoom, Message
 from users.models import LitterUser
@@ -11,9 +12,9 @@ class ChatConsumer(WebsocketConsumer):
     def create_chat(self, msg, pk):
         room = ChatRoom.objects.get(id=self.room_group_name)
         sender = LitterUser.objects.get(id=pk)
-        Message.objects.create(chatroom=room,
-                               user=sender,
-                               text=msg)
+        return Message.objects.create(chatroom=room,
+                                      user=sender,
+                                      text=msg)
 
     def connect(self):
         self.room_group_name = self.scope['url_route']['kwargs']['pk']
@@ -29,21 +30,25 @@ class ChatConsumer(WebsocketConsumer):
         sender = text_data_json['pk']
         message = text_data_json['message']
 
-        self.create_chat(message, sender)
-
+        message = self.create_chat(message, sender)
+        msg = str(message.text)
+        sender = str(message.user.username)
+        sent = str(timesince.timesince(message.sent))
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': msg,
+                'sender': sender,
+                'sent': sent
             }
         )
 
     def chat_message(self, event):
-        message = event['message']
-
         self.send(text_data=json.dumps({
             'type': 'chat',
-            'message': message
+            'message': event['message'],
+            'sender': event['sender'],
+            'sent': event['sent']
         }))
 # TODO make the connection not timeout as long as user is on the page
