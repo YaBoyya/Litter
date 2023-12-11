310a1da9af8c2b3a3bcad720c708498f45fad280
YaBoyya <duzniak.amadeusz@gmail.com>
2023-12-11 20:47:30 +0100

    Add all models to admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+admin.site.register(models.Language)
+admin.site.register(models.Post)
+admin.site.register(models.PostVote)
+admin.site.register(models.Comment)
+admin.site.register(models.CommentVote)
