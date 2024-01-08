e0f575997b7e0c6a1809c7ca83cd31553adba3df
P4C2UR <dpaczurb@gmail.com>
2024-01-08 10:41:39 +0100

    Frontend side ajax for notificaton-list

---
+++ profiles/static/js/notification-list.js
@@ -0,0 +1,22 @@
+function deleteReadE() {
+  function display() {
+    read = document.getElementById("main")
+      .getElementsByClassName("container-list")[0]
+      .querySelectorAll("li:not(highlight-container)")
+    for(let notification of read) {
+      notification.remove()
+    }
+  }
+  ajax(display, "GET", "notifications/delete-read")
+}
+function readAllE() {
+  function display() {
+    unread = document.getElementById("main")
+      .getElementsByClassName("container-list")[0]
+      .getElementsByClassName("highlight-container")
+    for(let notification of unread) {
+      notification.classList.remove("highlight-container")
+    }
+  }
+  ajax(display, "GET", "notifications/read-all")
+}
