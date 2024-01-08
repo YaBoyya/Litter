ced5b8b9a5b897a902c350a54d94a126cd8e1f8b
P4C2UR <dpaczurb@gmail.com>
2024-01-08 16:00:32 +0100

    Update notification counter when deleting or reading them

--- profiles/static/js/notification-list.js
+++ profiles/static/js/notification-list.js
@@ -1,3 +1,35 @@
+function getNotificationCount() {
+  lis = document.getElementById("nav-list")
+    .getElementsByTagName("li")
+  for(let li of lis) {
+    let anchors = li.getElementsByTagName("a")
+    if(anchors.length > 0 &&
+       anchors[0].href.indexOf("notification") != -1) {
+      return anchors[0]
+    }
+  }
+}
+
+function setNotification(val) {
+  a = getNotificationCount()
+  let text = a.innerHTML.trim().split(" | ")
+  if(val == 0) {
+    a.innerHTML = text[0]
+  } else {
+    a.innerHTML = text[0] + " | " + val
+  }
+}
+
+function subtractNotification(val) {
+  a = getNotificationCount()
+  let text = a.innerHTML.trim().split(" | ")
+  if(Number(text[1])-val == 0) {
+    a.innerHTML = text[0]
+  } else {
+    a.innerHTML = text[0] + " | " + (Number(text[1])-val)
+  }
+}
+
 function deleteReadE() {
   function display() {
     let read = document.getElementById("main")
@@ -18,6 +50,7 @@ function readAllE() {
     while(unread.length) {
       unread[0].classList.remove("highlight-container")
     }
+    setNotification(0)
   }
   ajax(display, "GET", "notifications/read-all")
 }
@@ -25,6 +58,8 @@ function readAllE() {
 function deleteNotificationE(id) {
   let t = event.target.parentNode.parentNode
   function display() {
+    if(t.classList.contains("highlight-container"))
+      subtractNotification(1)
     t.remove()
   }
   ajax(display, "GET", "notifications/" + id + "/delete")
