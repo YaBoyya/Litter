a3e2aba693f67aec02c24ca4967211b1d0e4dc26
YaBoyya <duzniak.amadeusz@gmail.com>
2024-01-16 21:35:06 +0100

    Specify default settings file

--- manage.py
+++ manage.py
@@ -3,10 +3,15 @@
 import os
 import sys
 
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Litter.settings.local_settings')  # noqa
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Litter.settings.deployment')  # noqa
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
